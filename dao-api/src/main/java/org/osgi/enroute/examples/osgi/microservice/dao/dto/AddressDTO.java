cddc79742b87df262d3b43087f603a1c54b1e2dc
Alvaro Denis <denisacostaq@gmail.com>
2021-01-26 17:07:01 +0200

    create dao api

---
+++ dao-api/src/main/java/org/osgi/enroute/examples/osgi/microservice/dao/dto/AddressDTO.java
@@ -0,0 +1,9 @@
+package org.osgi.enroute.examples.osgi.microservice.dao.dto;
+
+public class AddressDTO {
+
+	public long personId;
+	public String emailAddress;
+	public String city;
+	public String country;
+}
