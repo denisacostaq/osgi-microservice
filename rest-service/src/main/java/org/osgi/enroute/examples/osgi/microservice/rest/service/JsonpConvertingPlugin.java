cda5db84c48378bc0004eeb9efc05ef46ebbee5e
Alvaro Denis <denisacostaq@gmail.com>
2021-01-26 17:52:23 +0200

    Rest service

---
+++ rest-service/src/main/java/org/osgi/enroute/examples/osgi/microservice/rest/service/JsonpConvertingPlugin.java
@@ -0,0 +1,5 @@
+package org.osgi.enroute.examples.osgi.microservice.rest.service;
+
+public class JsonpConvertingPlugin {
+
+}
---
+++ rest-service/src/test/java/org/osgi/enroute/examples/osgi/microservice/rest/service/UnitTest.java
@@ -0,0 +1,12 @@
+package org.osgi.enroute.examples.osgi.microservice.rest.service;
+
+import org.junit.Test;
+
+public class UnitTest {
+    
+    @Test
+    public void testSomething() {
+        //TODO add an implementation
+    }
+    
+}
